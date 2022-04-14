5292842e733dc4cc170c4d107a9e6ed9fa4ddbb3
soelvi <solvisimensen@live.no>
2022-04-14 14:50:25 +0200

    added api

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,24 @@
 <template>
 	<div class="weather">
 		<p class="weather__title">  {{ title }} </p>
-		<h1 class="weather__location"> Oslo </h1>
+		<h1 class="weather__location"> insert city here (bergen) </h1>
 
+	<div class="weather_days">
 		<section class="weather__today">
-
+			today: 
+			{{ weather.temperature }}
+			{{ weather.wind }}
+			{{ weather.description }}
 		</section>
-		
+		<section class="weather__tomorrow">
+			tomorrow: 
+			{{ weather.forecast[0] }}
+		</section>
+		<section class="weather__afterTomorrow">
+			day after tomorrow: 
+			{{ wa }}
+		</section>
+	</div>
 	</div>
 </template>
 
@@ -14,7 +26,12 @@
 export default {
 	data() {
 		return {
-			title: 'Whats the weather in:'
+			title: 'Whats the weather in:',
+			weather: {},
+			error: '',
+			days: [
+			"today", "tomorrow", "afterTomorrow"
+			]
 		}
 	},
 
@@ -24,7 +41,20 @@ export default {
 
 	methods: {
 		async fetchData() {
-			const url = ``
+			const url = `https://goweather.herokuapp.com/weather/bergen`;
+			const response = await fetch(url);
+			try {
+				await this.handleResponse(response);
+			} catch (error) {
+				this.error = error.message;
+			}
+		},
+
+		async handleResponse(response) {
+			if (response.status >= 200 && response.status < 300) {
+				const results = await response.json();
+				this.weather = results;
+			}
 		}
 	}
 }
